c229474f1d197337d02754d5fce7c0f011f99062
Aakash Goswami <63198499+ninjaOfcode@users.noreply.github.com>
2021-07-17 02:09:41 +0530

    Create Count subset with given sum.cpp

---
+++ Count subset with given sum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>&arr,int n,int sum)
+    {
+        if(sum==0)
+            return 1;
+        if(sum<0)
+            return 0;
+        if(n<0)
+            return 0;
+            
+        int c1 = solve(arr,n-1,sum);
+        int c2 = solve(arr,n-1,sum-arr[n]);
+        return c1+c2;
+    }
+    
+int main()
+    {
+        vector<int>arr = {12,6,14,9,4,8,2,10,3};
+        int sum = 1;
+        int n = arr.size();
+        cout<<solve(arr,n-1,sum)<<endl;
+        return 0;
+    }
